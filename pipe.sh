328a6118555b390d69f9e23619c843b6a433f2e6
Caro Appleby <caroline.appleby@cantab.net>
2023-06-23 12:18:40 +0100

    add a thing to type |

---
+++ pipe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title pipe
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description just type the | character, because my keyboard doesn't have
+# @raycast.author Caro
+# @raycast.authorURL https://github.com/caro401
+
+echo '|' | pbcopy
